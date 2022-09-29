43930ce6808828f4986ff7d6d2fb66829142e8e5
Partha1999 <impartha1999@gmail.com>
2022-09-29 23:26:22 +0600

    Card edit

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,10 +2,10 @@ import React from 'react';
 const Product = (props) => {
     const {img, title, describe, age, time} = props.product;
     return (
-        <div className='product'>
+        <div className='product bg-white p-4'>
             <img className='w-full h-[200px]' src={img} alt=""/>
             <div className='product-info'>
-                <h1 className='product-name'>{title}</h1>
+                <h1 className='product-name text-2xl font-semibold'>{title}</h1>
                 <p>{describe}</p>
                 <h5>For age: {age}s</h5>
                 <h5>Time required: {time}s</h5>
